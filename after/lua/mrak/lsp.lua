ffd05cb2925e841ef865bbe26ac2a6d68cfc10e8
Eric Mrak <eric.mrak@fastly.com>
2025-04-29 17:05:36 -0700

    lua_ls

--- after/lua/mrak/lsp.lua
+++ after/lua/mrak/lsp.lua
@@ -8,6 +8,7 @@ if vim.fn.executable('pyright') == 1              then vim.lsp.enable('pyright')
 if vim.fn.executable('terraform-ls') == 1         then vim.lsp.enable('terraformls') end
 if vim.fn.executable('gopls') == 1                then vim.lsp.enable('gopls')       end
 if vim.fn.executable('pest-language-server') == 1 then vim.lsp.enable('pest_ls')     end
+if vim.fn.executable('lua-language-server') == 1  then vim.lsp.enable('lua_ls')      end
 
 local lsp_augroup = vim.api.nvim_create_augroup('Mrak#LSP', {clear = true})
 vim.api.nvim_create_autocmd('LspAttach', {
